9b18dd1caf96981570fe5b4a1da02e5cb47f4196
Mahmoud-Ibrahim-750 <104442642+Mahmoud-Ibrahim-750@users.noreply.github.com>
2024-05-09 19:51:31 +0300

    Implemented Splash Screen
    * implemented a basic splash screen

--- app/src/main/java/com/mis/route/e_commerce/ui/activities/home/HomeActivity.kt
+++ app/src/main/java/com/mis/route/e_commerce/ui/activities/home/HomeActivity.kt
@@ -1,11 +1,12 @@
-package com.mis.route.e_commerce
+package com.mis.route.e_commerce.ui.activities.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.mis.route.e_commerce.R
 
-class MainActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_home)
     }
 }
